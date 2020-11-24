b197faf2cd253e7edc747e2c5880880e44475852
Apollo1207 <fednyakillya12072001@gmail.com>
2020-11-24 19:24:14 +0200

    update structure of project

--- classes/Graph.py
+++ classes/Graph.py
@@ -9,18 +9,12 @@ class Graph:
             raise TypeError('All the params must be "Integer"')
         if weight < 0:
             raise ValueError('Weight can`t be negative')
-            # raise ValueError('edge weight can`t be negative')
-        # create vertex
         self.__create_edge(first_vertex_id, second_vertex_id, weight)
-        # create vertex in_files reverse order
         self.__create_edge(second_vertex_id, first_vertex_id, weight)
 
     def __create_edge(self, vertex_from: int, vertex_to: int, weight: int):
-        # check if vertex not exists already
         if vertex_from in self.connections.keys():
-            # check if the connection between these two vertexes not exists already
             if (vertex_to, weight) not in self.connections[vertex_from]:
-                # add connection from 'vertex_from' to 'vertex_to with' weight 'weight'
                 self.connections[vertex_from].append((vertex_to, weight))
         else:
             self.connections[vertex_from] = [(vertex_to, weight)]
